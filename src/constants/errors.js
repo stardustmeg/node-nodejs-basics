887500f06eaee6cc61ad3601c585bb1a05719f0c
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-09-26 00:43:05 +0300

    refactor: move errors to a common constant

---
+++ src/constants/errors.js
@@ -0,0 +1,3 @@
+export const errors = {
+  FILE_EXISTS: 'FS operation failed: file already exists',
+};
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,16 +1,17 @@
 import { writeFile, access, constants } from 'node:fs/promises';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { errors } from '../constants/errors.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const filePath = path.join(__dirname, 'files', 'fresh.txt');
+
 const CONTENT = 'I am fresh and young';
-const FILE_EXISTS_ERROR_TEXT = 'FS operation failed: file already exists';
 
 const create = async () => {
   try {
     await access(filePath, constants.F_OK);
-    console.error(FILE_EXISTS_ERROR_TEXT);
+    console.error(errors.FILE_EXISTS);
   } catch (err) {
     if (err.code === 'ENOENT') {
       await writeFile(filePath, CONTENT);
