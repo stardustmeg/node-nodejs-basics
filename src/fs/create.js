92a69c3ebfe0f5a8296fc3401c4706849ac67bca
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-09-25 23:35:28 +0300

    feat: create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { writeFile, access, constants } from 'node:fs/promises';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, 'files', 'fresh.txt');
+const CONTENT = 'I am fresh and young';
+const FILE_EXISTS_ERROR_TEXT = 'FS operation failed: file already exists';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await access(filePath, constants.F_OK);
+    console.error(FILE_EXISTS_ERROR_TEXT);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      await writeFile(filePath, CONTENT);
+    } else {
+      console.log(err);
+    }
+  }
 };
 
 await create();
