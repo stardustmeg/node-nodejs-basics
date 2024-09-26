4515fe84a6c52e41c13d438ee7678a9bfb4d2002
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-09-26 03:38:03 +0300

    refactor: create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,23 +1,41 @@
 import { writeFile, access, constants } from 'node:fs/promises';
-import path, { dirname } from 'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
-import { errors } from '../constants/errors.js';
+import { ERROR_MESSAGE } from '../constants/errors.js';
+import { CONTENT } from '../constants/file_content.js';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-const filePath = path.join(__dirname, 'files', 'fresh.txt');
+const getFilePath = () => {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  return path.join(__dirname, 'files', 'fresh.txt');
+};
 
-const CONTENT = 'I am fresh and young';
-
-const create = async () => {
+const fileExists = async (filePath) => {
   try {
     await access(filePath, constants.F_OK);
-    console.error(errors.FILE_EXISTS);
+    return true;
   } catch (err) {
     if (err.code === 'ENOENT') {
-      await writeFile(filePath, CONTENT);
-    } else {
-      console.log(err);
+      return false;
     }
+    throw err;
+  }
+};
+
+const createFile = async (filePath) => {
+  await writeFile(filePath, CONTENT);
+};
+
+const create = async () => {
+  const filePath = getFilePath();
+
+  try {
+    if (await fileExists(filePath)) {
+      throw new Error(ERROR_MESSAGE);
+    } else {
+      await createFile(filePath);
+    }
+  } catch (err) {
+    throw new Error(ERROR_MESSAGE);
   }
 };
 
