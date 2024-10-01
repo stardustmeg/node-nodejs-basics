0e12bd81fbfcb89768329e8f3a97137caf17a45c
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-10-01 16:45:53 +0300

    feat: delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { ERROR_MESSAGE } from '../constants/errors.js';
+
+const getDirPath = (dirName) => {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  return path.join(__dirname, dirName);
+};
+
 const remove = async () => {
-    // Write your code here 
+  const filePath = path.join(getDirPath('files'), 'fileToRemove.txt');
+  try {
+    await fs.unlink(filePath);
+  } catch (err) {
+    if (err.code === 'ENOENT') {
+      throw new Error(ERROR_MESSAGE);
+    }
+    throw err;
+  }
 };
 
 await remove();
