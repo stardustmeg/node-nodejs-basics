5fbaf798577e550eadd7492a8f474cba84918ebb
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-10-01 17:21:51 +0300

    feat: list files

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import { readdir, access, constants } from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { ERROR_MESSAGE } from '../constants/errors.js';
+
+const MODULE_DIRECTORY = path.dirname(fileURLToPath(import.meta.url));
+const filesFolder = path.join(MODULE_DIRECTORY, 'files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    await access(filesFolder, constants.R_OK);
+    const filenames = await readdir(filesFolder);
+    console.log(filenames);
+  } catch (error) {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await list();
