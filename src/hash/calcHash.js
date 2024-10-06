b4a6c02c604d3263c3a4baf4138dbd1381781bb3
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-10-06 20:05:18 +0300

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { createHash } from 'crypto';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filesFolderName = 'files';
+const file = join(__dirname, filesFolderName, 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  const readStream = createReadStream(file);
+
+  readStream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  readStream.on('end', () => {
+    const hex = hash.digest('hex');
+    console.log(hex);
+  });
 };
 
 await calculateHash();
