f46c0b355178fd7c41c53f6b39bbc26d6c0cc8bb
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-10-06 16:53:19 +0300

    feat: read file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { readFile } from 'node:fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { ERROR_MESSAGE } from '../constants/errors.js';
+
+const MODULE_DIRECTORY = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(MODULE_DIRECTORY, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const content = await readFile(filePath, 'utf8');
+    console.log(content);
+  } catch (error) {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await read();
