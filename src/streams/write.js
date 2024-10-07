d50a43744a2b9e5b74df9efec193273ef998fecf
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-10-07 18:27:40 +0300

    feat: write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import { once } from 'node:events';
+import fsPromises from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { ERROR_MESSAGE } from '../constants/errors.js';
+
+const MODULE_DIRECTORY = path.dirname(fileURLToPath(import.meta.url));
+const file = path.resolve(MODULE_DIRECTORY, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+  try {
+    const handle = await fsPromises.open(file, 'w');
+    const writeStream = handle.createWriteStream();
+
+    process.stdin.pipe(writeStream);
+
+    await once(writeStream, 'finish');
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await write();
