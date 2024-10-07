a8e4d285ff770247c022824cb88d9a5c9e832eef
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-10-07 18:41:44 +0300

    feat: transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from 'node:stream';
+import os from 'node:os';
+
 const transform = async () => {
-    // Write your code here 
+  const reverseStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedChunk = chunk.toString().trimEnd().split('').reverse().join('');
+      callback(null, `${reversedChunk}${os.EOL}`);
+    },
+  });
+
+  try {
+    process.stdin.pipe(reverseStream).pipe(process.stdout);
+  } catch (err) {
+    console.error(`An error occured: ${err}`);
+    process.exitCode = 1;
+  }
 };
 
-await transform();
+transform();
