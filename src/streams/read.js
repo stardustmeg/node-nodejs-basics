891f6e13c3d65489860311bfcf2c4979708c5e8c
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-10-07 18:19:57 +0300

    feat: read stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import { once } from 'node:events';
+import fsPromises from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import { ERROR_MESSAGE } from '../constants/errors.js';
+
+const MODULE_DIRECTORY = path.dirname(fileURLToPath(import.meta.url));
+const file = path.resolve(MODULE_DIRECTORY, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const handle = await fsPromises.open(file);
+    const readStream = handle.createReadStream();
+    const stream = readStream.pipe(process.stdout);
+
+    await once(stream, 'finish');
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await read();
